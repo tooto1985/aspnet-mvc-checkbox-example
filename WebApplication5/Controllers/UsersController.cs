a445be0ba7ba96d94334c3fe0e43adac2b783f7b
tommy <tooto1985@gmail.com>
2015-11-19 09:36:43 +0800

    aspnet-mvc-checkbox-example

---
+++ WebApplication5/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(List<string> qoo)
+        {
+            @ViewBag.Selected = qoo.Where(x => x != "false").ToList();
+            return View();
+            
+        }
+
+    }
+}
